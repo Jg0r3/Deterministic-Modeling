d21646e3effca72d393059f01b42a3e63d4b47c5
JonathanGore <43797248+Jg0r3@users.noreply.github.com>
2019-03-31 15:39:00 -0600

    Create BacterialGrowth.m
    created bacterial growth script for homework solution

---
+++ HW 1/BacterialGrowth.m
@@ -0,0 +1,19 @@
+% Problem 1.4.2
+% modelling bacterial growth
+format longG %convert 1+e0 to more readable numbers for troubleshooting
+
+N = input("input end of range of growth function: ")
+powerof2(1) = 1;         %initialization (zero power)
+for i = 1:N              % same as for i = 1:1:N
+    powerof2(i+1) = 2^i; %statement
+end                      %end of "for" loop 
+
+arraylength = [1:1:(N+1)];
+
+powerof2'
+arraylength'
+
+figure(1)
+  plot(arraylength, powerof2, 'r')
+
+clear all
