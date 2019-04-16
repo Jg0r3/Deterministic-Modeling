0276fe41f160699f0fa977772051ae9fd96053ef
Jonathan-Gore <43797248+Jonathan-Gore@users.noreply.github.com>
2019-04-16 15:00:10 -0600

    more changes, no progreessss

--- scratchpaper.m
+++ scratchpaper.m
@@ -1,9 +1,32 @@
 
-syms f x g b
+% syms f x g b
+% 
+% f(x) = 2*x - b + x
+% 
+% g(x) = f(x) - x
+% 
+% v = fzero(g(x),1)
 
-f(x) = 2*x - b + x
+%A(n+1) = u1*A(n) - u3*A(n)*B(n)
+%B(n+1) = u3*B(n) - u4*A(n)*B(n)
+syms x k b
+%eqn = 1 == (b - k)/x ;
+eqn = x == x-(k*x)+b
+sol = solve(eqn,x)
 
-g(x) = f(x) - x
+f = x-(k*x)+b
+fplot(x)
+% syms x y u1 u2 u3 u4
+% cond1 = x^2 + y^2 + x*y < 1;
+% cond2 = x > 0;
+% cond3 = y > 0;
+% conds = [cond1 cond2 cond3];
+% 
+% sol = solve(conds, [x y], 'ReturnConditions', true);
 
-v = fzero(g(x),1)
+% sol.x
+% sol.y
+% sol.parameters
+% sol.conditions
 
+clear all
