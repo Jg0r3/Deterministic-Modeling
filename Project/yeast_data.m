8c4cb8bf6ef58a9d4d1e0b48efc68d57a2c91856
JonathanGore <43797248+Jg0r3@users.noreply.github.com>
2019-04-24 18:03:37 -0600

    Create yeast_data.m

---
+++ Project/yeast_data.m
@@ -0,0 +1,31 @@
+
+%----------------------------------%
+% Data                             %
+%----------------------------------%
+%
+%C_0=0.02; provided initial condition from research
+%
+% K1 Run
+% Vessel 1 Vessel Volumes/Hr
+K_vvh_1 = [0.028 0.099 0.142 0.207 0.269 0.287 0.352 0.403]';
+% Vessel 1 Optical Density at the steady state
+K_OD_1 = [0.144 0.151 0.099 0.069 0.045 0.02 0.003 0]';
+
+% Vessel 2 Vessel Volumes/Hr
+K_vvh_2 = [0.054 0.11 0.141 0.199 0.257 0.296 0.348 0.397 0.41]';
+% Vessel 2 Optical Density at the steady state
+K_OD_2 = [0.164 0.151 0.11 0.092 0.072 0.023 0.006 0.002 0.004]';
+
+
+%-----------------------------------
+
+% Sensitive- Run
+% Vessel 1 Vessel Volumes/Hr
+S_vvh_1 = [0.041 0.099 0.167 0.223 0.266 0.328 0.356 0.401 0.462]';
+% Vessel 1 Optical Density at the steady state %% Bacterial population
+S_OD_1 = [0.54 0.494 0.459 0.395 0.229 0.019 0.006 0.003 0]';
+
+% Vessel 2 Vessel Volumes/Hr
+S_vvh_2 = [0.0571 0.126 0.196 0.263 0.313 0.383]';
+% Vessel 2 Optical Density at the steady state
+S_OD_2 = [0.385 0.456 0.363 0.197 0.044 0.004]';
